d01ac47f8741d058cf34545a60f7cc448dca2593
VardanzZ <dnngyy@gmail.com>
2024-11-07 13:01:26 +0700

    first commit

---
+++ src/main.js
@@ -0,0 +1,31 @@
+import { LibraryController } from "./controllers/LibraryController.js";
+import { BookForm } from "./views/BookForm.js";
+import { BookList } from "./views/BookList.js";
+
+const bookForm = new BookForm();
+const bookList = new BookList();
+const libraryController = new LibraryController(bookForm, bookList);
+
+// Menambahkan event listener pada form
+bookForm.render((title, author, year) => {
+  libraryController.addBook(title, author, year);
+  // Setelah buku ditambahkan, update daftar buku
+  bookList.render(
+    libraryController.books, 
+    libraryController.deleteBook.bind(libraryController),
+    libraryController.editBook.bind(libraryController)
+  );
+});
+
+// Menampilkan daftar buku awal
+bookList.render(
+  libraryController.books, 
+  libraryController.deleteBook.bind(libraryController),
+  libraryController.editBook.bind(libraryController)
+);
+
+// Tema
+document.getElementById("theme-toggle")
+  .addEventListener("click", () => libraryController.toggleTheme());
+
+libraryController.initTheme();
